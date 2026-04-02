daea012d2c8044fbe7372af6447c5defafecd8b9
Vincent Clausing-Lage <vincent.clausing.lage@gmail.com>
2026-04-02 07:10:07 +0200

    MOD size

--- Scripts/World/killerfish.gd
+++ Scripts/World/killerfish.gd
@@ -4,7 +4,7 @@ extends RigidBody2D
 func _ready() -> void:
 	print('fish ready')
 	print(self.position.y)
-	self.apply_impulse(Vector2(-9, 0))
+	self.apply_impulse(Vector2(-2, 0))
 
 
 func _process(delta: float) -> void:
